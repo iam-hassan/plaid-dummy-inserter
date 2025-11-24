dd5b4a3f48a3945f88774500f578e7c310be2c48
Muhammad Hassan <149463639+iam-hassan@users.noreply.github.com>
2025-11-24 19:37:29 +0000

    idk2

--- add-data.js
+++ add-data.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       created_at: new Date().toISOString()
     });
   }
-
+  console.log("Inerted the data")
   const { data, error } = await supabase
     .from('plaid_items')
     .insert(dummyRows);
